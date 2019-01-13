7088c02f743bb2b178f63d22a803fc89cdd679c1
bigbear <xiongzhengyu@hqyxjy.com>
2019-01-14 02:30:53 +0800

    move files

--- app/src/main/java/com/rubitree/demo/MainActivity.java
+++ app/src/main/java/com/rubitree/demo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.rubitree.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.rubitree.demo.nestedinnested.NestedInNestedActivity;
+import com.rubitree.demo.suspendedlayout.SuspendedLayoutActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.vToNestedInNested).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NestedInNestedActivity.class));
+            }
+        });
+
+        findViewById(R.id.vToSuspendedLayout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SuspendedLayoutActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/rubitree/demo/nestedinnested/NestedInNestedActivity.java
@@ -0,0 +1,16 @@
+package com.rubitree.demo.nestedinnested;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rubitree.demo.R;
+
+public class NestedInNestedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nested_in_nested);
+    }
+}
